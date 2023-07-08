a74a68395d176ab8857157fcb916c6550d7da73c
Ikram khan <ikr302000@gmail.com>
2023-07-08 12:17:31 +0500

    adding places with provider and displaying places

--- lib/views/screens/add_place.dart
+++ lib/views/screens/add_place.dart
@@ -1,14 +1,25 @@
+import 'package:favorite_places/Providers/user_places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AddPlaceScreen extends StatefulWidget {
+class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
 
   @override
-  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
 }
 
-class _AddPlaceScreenState extends State<AddPlaceScreen> {
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+    if (enteredTitle.isEmpty) {
+      return;
+    }
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    Navigator.of(context).pop();
+  }
 
   @override
   void dispose() {
@@ -39,7 +50,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
               height: 16,
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: _savePlace,
               icon: const Icon(Icons.add),
               label: const Text('Add Place'),
             ),
--- lib/views/screens/places.dart
+++ lib/views/screens/places.dart
@@ -1,12 +1,15 @@
+import 'package:favorite_places/Providers/user_places.dart';
 import 'package:favorite_places/views/screens/add_place.dart';
 import 'package:favorite_places/views/widgets/places_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Places"),
@@ -23,7 +26,9 @@ class PlacesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const PlacesList(places: []),
+      body: PlacesList(
+        places: userPlaces,
+      ),
     );
   }
 }
