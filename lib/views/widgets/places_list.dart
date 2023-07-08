8e22bd39eaf2db2019d6c00670c1691c6d30cb20
Ikram khan <ikr302000@gmail.com>
2023-07-08 18:46:02 +0500

    adding a place detail screen

--- lib/views/widgets/places_list.dart
+++ lib/views/widgets/places_list.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/views/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/place.dart';
@@ -21,13 +22,22 @@ class PlacesList extends StatelessWidget {
       );
     }
     return ListView.builder(
-        itemCount: places.length,
-        itemBuilder: (ctx, index) => ListTile(
-              title: Text(
-                places[index].title,
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground),
+      itemCount: places.length,
+      itemBuilder: (ctx, index) => ListTile(
+          title: Text(
+            places[index].title,
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => PlaceDetailScreen(place: places[index]),
               ),
-            ));
+            );
+          }),
+    );
   }
 }
