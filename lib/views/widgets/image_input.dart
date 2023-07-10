5b4e01f9f8834c9e79e66804f2842e62554c397a
Ikram khan <ikr302000@gmail.com>
2023-07-10 14:27:19 +0500

    using the device camera and gallery for picking images

--- lib/views/widgets/image_input.dart
+++ lib/views/widgets/image_input.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+enum PickImageSource {
+  camera,
+  gallery,
+}
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,24 +15,85 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+  XFile? pickedImage;
+
+  void _takePicture(PickImageSource source) async {
+    final imagePicker = ImagePicker();
+
+    if (source == PickImageSource.camera) {
+      pickedImage = await imagePicker.pickImage(
+        source: ImageSource.camera,
+        maxWidth: 600,
+      );
+    } else if (source == PickImageSource.gallery) {
+      pickedImage = await imagePicker.pickImage(
+        source: ImageSource.gallery,
+        maxWidth: 600,
+      );
+    }
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedImage = File(pickedImage!.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 1,
-          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+    Widget content = TextButton.icon(
+      onPressed: () {
+        _takePicture(PickImageSource.camera);
+      },
+      icon: const Icon(Icons.camera),
+      label: const Text('Take Picture'),
+    );
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: () {
+          _takePicture(PickImageSource.camera);
+        },
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
         ),
-      ),
-      height: 250,
-      width: double.infinity,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-      ),
+      );
+    }
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+            ),
+          ),
+          height: 250,
+          width: double.infinity,
+          child: content,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _takePicture(PickImageSource.gallery);
+          },
+          child: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.camera_alt),
+              SizedBox(width: 8.0),
+              Text('Upload Image'),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
