e6c20df20ea1e73e2f1bdc4f443685eb7f23305c
Ikram khan <ikr302000@gmail.com>
2023-09-22 23:53:07 +0500

    using google map API and google Geocoding

--- lib/views/widgets/location_input.dart
+++ lib/views/widgets/location_input.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'package:location/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../../api_key.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -39,13 +43,17 @@ class _LocationInputState extends State<LocationInput> {
       _isGettingLocation = true;
     });
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
 
+    final url = Uri.parse(
+        "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$gmapApiKey");
+    final response = await http.get(url);
+    final resData = json.decode(response.body);
+    final address = resData['results'][0]['formatted_address'];
     setState(() {
       _isGettingLocation = false;
     });
-
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
 
   @override
