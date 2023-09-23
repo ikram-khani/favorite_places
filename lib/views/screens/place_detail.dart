d2f6b8b44e8be8ac230c92a6865a5d2260cf84ae
Ikram khan <ikr302000@gmail.com>
2023-09-23 15:40:15 +0500

    outputting the location data

--- lib/views/screens/place_detail.dart
+++ lib/views/screens/place_detail.dart
@@ -1,9 +1,19 @@
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 
+import '../../api_key.dart';
+
 class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({super.key, required this.place});
   final Place place;
+
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap &markers=color:red%7Clabel:S%7C$lat,$lng&key=$gmapApiKey';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +27,42 @@ class PlaceDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                    colors: [
+                      Colors.transparent,
+                      Colors.black54,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  )),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
