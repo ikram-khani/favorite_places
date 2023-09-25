fa3c29fad3b6e0242c786f54c95f0b1643ec07c2
Ikram khan <ikr302000@gmail.com>
2023-09-25 12:19:20 +0500

    showing current location on map during selecting

--- lib/views/screens/map.dart
+++ lib/views/screens/map.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({
@@ -21,6 +24,58 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   LatLng? _pickedLocation;
+  var _isGettingLocation = false;
+  @override
+  void initState() {
+    super.initState();
+    //if it is selecting then get the usercurrent location in the initstate
+    if (widget.isSelecting) {
+      _getCurrentUserLocation();
+    }
+  }
+
+//get current location for the map
+  Future<void> _getCurrentUserLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+    //showing loading spinner during fetching current location
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    if (lat == null || lng == null) {
+      return;
+    }
+
+    setState(() {
+      _pickedLocation = LatLng(lat, lng);
+      _isGettingLocation = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,38 +92,45 @@ class _MapScreenState extends State<MapScreen> {
             ),
         ],
       ),
-      body: GoogleMap(
-        onTap: !widget.isSelecting
-            ? null
-            : (position) {
-                setState(() {
-                  _pickedLocation = position;
-                });
-              },
-        initialCameraPosition: CameraPosition(
-          target: LatLng(
-            widget.location.latitude,
-            widget.location.longitude,
-          ),
-          zoom: 16,
-        ),
-        markers: (_pickedLocation == null && widget.isSelecting)
-            ? {}
-            : {
-                Marker(
-                  markerId: const MarkerId('m1'),
+      //show progressIndicator while fetching the current location
+      body: _isGettingLocation
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : GoogleMap(
+              onTap: !widget.isSelecting
+                  ? null
+                  : (position) {
+                      setState(() {
+                        _pickedLocation = position;
+                      });
+                    },
+              //in detail screen the _pickedLocation will be null from getCurrentLocation cz in initstate the isSelecting will be false and also in onTap it will be null so we will see the already save location
+              initialCameraPosition: CameraPosition(
+                target: _pickedLocation ??
+                    LatLng(
+                      widget.location.latitude,
+                      widget.location.longitude,
+                    ),
+                zoom: 16,
+              ),
+              markers: (_pickedLocation == null && widget.isSelecting)
+                  ? {}
+                  : {
+                      Marker(
+                        markerId: const MarkerId('m1'),
 
-                  //also can use this syntax (_pickedLocation??LatLng(widget.location.latitude,widget.location.longitude))
+                        //also can use this syntax (_pickedLocation??LatLng(widget.location.latitude,widget.location.longitude))
 
-                  position: _pickedLocation != null
-                      ? _pickedLocation!
-                      : LatLng(
-                          widget.location.latitude,
-                          widget.location.longitude,
-                        ),
-                ),
-              },
-      ),
+                        position: _pickedLocation != null
+                            ? _pickedLocation!
+                            : LatLng(
+                                widget.location.latitude,
+                                widget.location.longitude,
+                              ),
+                      ),
+                    },
+            ),
     );
   }
 }
