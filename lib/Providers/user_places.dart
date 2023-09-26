298c5a9b4f18cb025ee192454861c765fbcfc7b0
Ikram khan <ikr302000@gmail.com>
2023-09-26 22:25:04 +0500

    using futureBuilder for loading data and fixing type error

--- lib/Providers/user_places.dart
+++ lib/Providers/user_places.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:favorite_places/models/place.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
@@ -13,7 +12,7 @@ Future<Database> _getDatabase() async {
     path.join(dbPath, 'places.db'),
     onCreate: (db, version) {
       return db.execute(
-          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL , lng TEXT, address TEXT)');
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
     },
     version: 1,
   );
@@ -23,7 +22,7 @@ Future<Database> _getDatabase() async {
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void loadPlaces() async {
+  Future<void> loadPlaces() async {
     final db = await _getDatabase();
     final data = await db.query('user_places');
     final places = data
